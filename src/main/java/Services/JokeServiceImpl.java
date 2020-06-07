4f2851f7f721d3a9fdda818445e5a11809501d5b
Lekang <lekangdu@gmail.com>
2020-06-06 20:43:40 -0400

    Created Services. Closed #1.

---
+++ src/main/java/Services/JokeServiceImpl.java
@@ -0,0 +1,19 @@
+package Services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chunkNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chunkNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return null;
+    }
+}
