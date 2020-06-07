c843989b31f2afff2830f1fcc53ba745609bb0c2
Lekang <lekangdu@gmail.com>
2020-06-06 21:01:15 -0400

    Created controller. Closed #2.

---
+++ src/main/java/Controllers/jokeController.java
@@ -0,0 +1,24 @@
+package Controllers;
+
+import Services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class jokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public jokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chunknorris";
+    }
+}
